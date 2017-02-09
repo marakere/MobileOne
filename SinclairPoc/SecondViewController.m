75b2071019c3cec12f683a5889a47518b139cac7
ArunPvt <aruunt@gmail.com>
2017-02-09 17:21:30 -0500

    AVPlayer Updates

--- SinclairPoc/SecondViewController.m
+++ SinclairPoc/SecondViewController.m
@@ -14,7 +14,10 @@
 
 @property(nonatomic, strong) AVPlayerViewController *controller;
 @property(nonatomic, strong) UIButton *playButton;
-
+@property(nonatomic, strong) AVPlayerItem *mainPlayerItem;
+@property(nonatomic) CMTime mainSTT;
+@property(nonatomic, strong) AVPlayerItem *breakingNewsPlayerItem;
+@property(nonatomic) BOOL iscalled;
 // SDK
 /// Entry point for the SDK. Used to make ad requests.
 @property(nonatomic, strong) IMAAdsLoader *adsLoader;
@@ -24,12 +27,23 @@
 
 /// Main point of interaction with the SDK. Created by the SDK as the result of an ad request.
 @property(nonatomic, strong) IMAAdsManager *adsManager;
+
+@property(nonatomic, strong) IMAAdDisplayContainer *adDisplayContainer;
+@property(nonatomic, strong) UIView *adView;
+
+//Temp
+@property(nonatomic, weak) IBOutlet UITextView *logView;
+@property(nonatomic, strong) NSMutableString *logText;
+
 @end
 
 @implementation SecondViewController
 
-    NSString *const VOD = @"http://rmcdn.2mdn.net/Demo/html5/output.mp4"; //@"https://s3-eu-west-1.amazonaws.com/alf-proeysen/Bakvendtland-MASTER.mp4";
+    NSString *const VOD = @"http://clips.vorwaerts-gmbh.de/VfE_html5.mp4";
+        //@"http://rmcdn.2mdn.net/Demo/html5/output.mp4";
+        //@"https://s3-eu-west-1.amazonaws.com/alf-proeysen/Bakvendtland-MASTER.mp4";
     NSString *const live = @"https://youtu.be/pCZeVTMEsik";
+    NSString *const breakingNews = @"http://techslides.com/demos/sample-videos/small.mp4";
 
     // Pre-roll
     //NSString *const kTestAppAdTagUrl = @"https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&"
@@ -91,6 +105,7 @@
                                              selector:@selector(contentDidFinishPlaying:)
                                                  name:AVPlayerItemDidPlayToEndTimeNotification
                                                object:self.controller.player.currentItem];
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -100,13 +115,73 @@
 
 -(void)onPlayButtonClick {
     
+    self.logText = [[NSMutableString alloc] initWithString:@"Main content video Started\n"];
+    [self.logView setText:self.logText];
+    
     if ([self.controller.player status] == AVPlayerStatusReadyToPlay) {
         
         [self requestAds];
         self.playButton.hidden = YES;
         self.controller.showsPlaybackControls = YES;
+        self.iscalled = NO;
+        [NSTimer scheduledTimerWithTimeInterval:1
+                                         target:self
+                                       selector:@selector(countDown)
+                                       userInfo:nil
+                                        repeats:YES];
     }
     
+}
+
+- (void) countDown {
+    
+//    NSLog(@"%f", self.controller.player.rate);
+//    NSLog(@"%@", self.controller.player.currentItem);
+//    NSLog(@"%f", CMTimeGetSeconds(self.controller.player.currentItem.currentTime));
+    if(self.controller.player.rate == 0) {
+        if(CMTimeGetSeconds(self.controller.player.currentItem.currentTime) < 1.00) {
+            if(![self.logText hasSuffix:@"Playing Pre-roll ads\n"]){
+                [self.logText appendString:@"Playing Pre-roll ads\n"];
+                self.logView.text = self.logText;
+            }
+           
+        } else if(CMTimeGetSeconds(self.controller.player.currentItem.currentTime) > 15.00 && !self.iscalled) {
+            if(![self.logText hasSuffix:@"Playing Mid-roll ads @ 15 sec\n"]){
+                [self.logText appendString:@"Playing Mid-roll ads @ 15 sec\n"];
+                self.logView.text = self.logText;
+            }
+            self.iscalled = YES;
+            self.mainPlayerItem = self.controller.player.currentItem;
+            self.mainSTT = self.controller.player.currentItem.currentTime;
+            [NSTimer scheduledTimerWithTimeInterval:5
+                                             target:self
+                                           selector:@selector(pauseAds)
+                                           userInfo:nil
+                                            repeats:NO];
+        } else if(CMTimeGetSeconds(self.controller.player.currentItem.currentTime) > 20.00) {
+            if(![self.logText hasSuffix:@"Playing Post-roll ads\n"]){
+                [self.logText appendString:@"Playing Post-roll ads\n"];
+                self.logView.text = self.logText;
+            }
+        }
+    }
+}
+
+- (void) pauseAds {
+    
+    [self.adsManager pause];
+    self.adView.hidden = YES;
+    [self.logText appendString:@"Mid-roll ad paused @ 5th sec\n"];
+    self.logView.text = self.logText;
+    self.breakingNewsPlayerItem = [AVPlayerItem playerItemWithURL:[NSURL URLWithString:breakingNews]];
+    [self.controller.player replaceCurrentItemWithPlayerItem:self.breakingNewsPlayerItem];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(contentDidFinishPlaying:)
+                                                 name:AVPlayerItemDidPlayToEndTimeNotification
+                                               object:self.controller.player.currentItem];
+    [self.controller.player play];
+    [self.logText appendString:@"Playing breaking content video\n"];
+    self.logView.text = self.logText;
 }
 
 #pragma mark SDK Setup
@@ -118,11 +193,13 @@
 
 - (void)requestAds {
     // Create an ad display container for ad rendering.
-    IMAAdDisplayContainer *adDisplayContainer =
-    [[IMAAdDisplayContainer alloc] initWithAdContainer:self.controller.view companionSlots:nil];
+    self.adView = [[UIView alloc] initWithFrame:self.controller.view.frame];
+    [self.view addSubview:self.adView];
+    self.adView.hidden = YES;
+    self.adDisplayContainer = [[IMAAdDisplayContainer alloc] initWithAdContainer:self.adView companionSlots:nil];
     // Create an ad request with our ad tag, display container, and optional user context.
     IMAAdsRequest *request = [[IMAAdsRequest alloc] initWithAdTagUrl:kTestAppAdTagUrl
-                                                  adDisplayContainer:adDisplayContainer
+                                                  adDisplayContainer:self.adDisplayContainer
                                                      contentPlayhead:self.contentPlayhead
                                                          userContext:nil];
     [self.adsLoader requestAdsWithRequest:request];
@@ -131,9 +208,16 @@
 
 - (void)contentDidFinishPlaying:(NSNotification *)notification {
     // Make sure we don't call contentComplete as a result of an ad completing.
-    if (notification.object == self.controller.player.currentItem) {
+    if (notification.object == self.controller.player.currentItem && notification.object == self.mainPlayerItem) {
         [self.adsLoader contentComplete];
         self.playButton.hidden = NO;
+        [self.logText appendString:@"Video ended\n"];
+        self.logView.text = self.logText;
+    } else if(notification.object == self.breakingNewsPlayerItem) {
+        [self.adsManager resume];
+        self.adView.hidden = NO;
+        [self.logText appendString:@"Resume Mid-roll ads from 5th sec\n"];
+        self.logView.text = self.logText;
     }
 }
 
@@ -175,11 +259,19 @@
 
 - (void)adsManagerDidRequestContentPause:(IMAAdsManager *)adsManager {
     // The SDK is going to play ads, so pause the content.
+    self.adView.hidden = NO;
     [self.controller.player pause];
 }
 
 - (void)adsManagerDidRequestContentResume:(IMAAdsManager *)adsManager {
     // The SDK is done playing ads (at least for now), so resume the content.
+    self.adView.hidden = YES;
+    if(self.mainPlayerItem && self.controller.player.currentItem != self.mainPlayerItem) {
+        [self.controller.player replaceCurrentItemWithPlayerItem:self.mainPlayerItem];
+        [self.controller.player.currentItem seekToTime:self.mainSTT];
+    }
     [self.controller.player play];
+    [self.logText appendString:@"Resume main content video after ads\n"];
+    self.logView.text = self.logText;
 }
 @end
